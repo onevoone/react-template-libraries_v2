bf438d059385ea2775975c79775f28c2c66132a6
vladimir.fedorchenko <vfedorchenko@softkit.company>
2021-04-19 17:05:04 +0300

    upd tsdx

--- tsdx.config.js
+++ tsdx.config.js
@@ -6,52 +6,13 @@ const alias = require('@rollup/plugin-alias');
 const postcss = require('postcss');
 const { nodeResolve } = require('@rollup/plugin-node-resolve');
 
-const customResolver = nodeResolve({
-	extensions: ['.mjs', '.js', '.jsx', '.json', '.sass', '.scss']
-});
-
-const projectRootDir = path.resolve(__dirname);
-
 module.exports = {
 	rollup(config, options) {
-
 		config.plugins.push(
-			// alias({
-			// 	entries: [
-			// 		{
-			// 			find: 'src',
-			// 			replacement: path.resolve(projectRootDir, 'src')
-			// 			// OR place `customResolver` here. See explanation below.
-			// 		}
-			// 	],
-			// 	customResolver,
-			// }),
-			// nodeResolve(),
 			scss({
-				// output: false,
-
-				// Filename to write all styles to
-				// output: 'dist/index.css',
-
-				// processor: () => postcss([autoprefixer({ overrideBrowserslist: 'Edge 18' })]),
-				processor: css => postcss([autoprefixer])
-					.process(css)
-					.then(result => result.css)
+				output: 'dist/index.css',
 			}),
 		);
-		// 	postcss({
-		// 		modules: true,
-		// 		plugins: [
-		// 			autoprefixer(),
-		// 			cssnano({
-		// 				preset: 'default',
-		// 			}),
-		// 		],
-		// 		inject: false,
-		// 		// only write out CSS for the first bundle (avoids pointless extra files):
-		// 		extract: !!options.writeMeta,
-		// 	})
-		// );
 		return config;
 	},
 };
