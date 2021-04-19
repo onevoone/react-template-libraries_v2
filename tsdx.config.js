1aeabab280f24e12e037b7c1509868b289837824
vladimir.fedorchenko <vfedorchenko@softkit.company>
2021-04-19 16:21:42 +0300

    upd

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,16 +1,43 @@
-const postcss = require('rollup-plugin-postcss');
+const path = require('path');
 const scss = require('rollup-plugin-scss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const alias = require('@rollup/plugin-alias');
+const postcss = require('postcss');
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
+
+const customResolver = nodeResolve({
+	extensions: ['.mjs', '.js', '.jsx', '.json', '.sass', '.scss']
+});
+
+const projectRootDir = path.resolve(__dirname);
 
 module.exports = {
 	rollup(config, options) {
+
 		config.plugins.push(
-			scss(),
-			// autoprefixer(),
-			// cssnano({
-			// 	preset: 'default',
+			// alias({
+			// 	entries: [
+			// 		{
+			// 			find: 'src',
+			// 			replacement: path.resolve(projectRootDir, 'src')
+			// 			// OR place `customResolver` here. See explanation below.
+			// 		}
+			// 	],
+			// 	customResolver,
 			// }),
+			// nodeResolve(),
+			scss({
+				// output: false,
+
+				// Filename to write all styles to
+				// output: 'dist/index.css',
+
+				// processor: () => postcss([autoprefixer({ overrideBrowserslist: 'Edge 18' })]),
+				processor: css => postcss([autoprefixer])
+					.process(css)
+					.then(result => result.css)
+			}),
 		);
 		// 	postcss({
 		// 		modules: true,
